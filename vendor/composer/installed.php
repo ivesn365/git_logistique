c6115c60d41ee291fefe9846b943c6a76a65400e
Yves Ngalamulume <yvesn314@gmail.com>
2026-04-21 10:04:35 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ccet/logistique',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c2e06bd2264cb446269efe972dc1a073f4e82afb',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ccet/logistique' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c2e06bd2264cb446269efe972dc1a073f4e82afb',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
